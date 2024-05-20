f20d49e7bf7bf8a29faa9b9c5a5e80f927e0b9fd
Thanushan <72187742+uvsq21919161@users.noreply.github.com>
2024-05-20 13:22:30 +0200

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,11 @@ import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import Comott from "../assets/Comott.png";
+import tableau from "../assets/icon/tableau_de_bord.png";
+import vote from "../assets/icon/vote.png";
+import guide from "../assets/icon/guide.png";
+import visualisation from "../assets/icon/visualisation.png";
+import deco from "../assets/icon/deconnexion.png";
 import { SidebarContext } from "./Sidebarprovider";
 import { UserContext } from "../../context/usercontext";
 
@@ -49,23 +54,23 @@ function Sidebar() {
         <img className="logosidebar" src={Comott} />
         <ul className="menu">
           <li className= {selectedMenu === "Tableau" ? "menu-item selecteditem" : "menu-item"} onClick={() => handleMenuClick("Tableau")}>
-            <img className="icon" src="src/assets/icon/tableau_de_bord.png" />
+            <img className="icon" src={tableau} />
             {isSideBarExpanded && <span className={selectedMenu === "Tableau" ? "item selected" : "item"}>Tableau de bord</span>}
           </li>
           <li className= {selectedMenu === "Election" ? "menu-item selecteditem" : "menu-item"} onClick={() => handleMenuClick("Election")}>
-            <img className="icon" src="src/assets/icon/vote.png"/>
+            <img className="icon" src={vote} />
             {isSideBarExpanded && <span className={selectedMenu === "Election" ? "item selected" : "item"}>Election du représentant</span>}
           </li>
           <li className= {selectedMenu === "Guide" ? "menu-item selecteditem" : "menu-item"} onClick={() =>handleMenuClick("Guide")}>
-          <img className="icon" src="src/assets/icon/guide.png"/>
+          <img className="icon" src={guide}/>
             {isSideBarExpanded && <span className={selectedMenu === "Guide" ? "item selected" : "item"}>Guide de l'électeur</span>}
           </li>
           <li className={selectedMenu === "Visu" ? "menu-item selecteditem" : "menu-item"} onClick={() => handleMenuClick("Visu")}>
-          <img className="icon" src="src/assets/icon/visualisation.png"/>
+          <img className="icon" src={visualisation}/>
             {isSideBarExpanded && <span className={selectedMenu === "Visu" ? "item selected" : "item"}>Visualisation des serveurs</span>}
           </li>
           <li className={selectedMenu === "Déconnexion" ? "menu-item selecteditem" : "menu-item"} onClick={() => handleDeco()}>
-          <img className="icon" src="src/assets/icon/deconnexion.png"/>
+          <img className="icon" src={deco}/>
             {isSideBarExpanded && <span className={selectedMenu === "Déconnexion" ? "item selected" : "item"}>Déconnexion</span>}
           </li>
         </ul>
